f8e60542633f144338dbe2898e86af29f74c0264
Anjali <anjalisharmahimawari@gmail.com>
2026-03-14 00:36:48 +0530

    Static files configuration

--- myproject/settings.py
+++ myproject/settings.py
@@ -139,3 +139,4 @@ LOGIN_URL = '/login/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
